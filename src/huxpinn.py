66405dc2fc49b55af9151fa7a4722afa6ad02375
BogdanM1 <dd11eeaann@gmail.com>
2021-11-16 18:51:01 +0000

    fixed bug with ifs, only analytical functions allowed

--- src/huxpinn.py
+++ src/huxpinn.py
@@ -18,7 +18,7 @@ sn.set_random_seed(_seed)
 
 
 ''' fixed parameters ''' 
-TOL = 1e-9
+TOL = 1e-3
 f1_0 = 43.3 
 h = 15.6
 g1 = 10.0
@@ -28,6 +28,10 @@ L0 = 1100.0
 dt = 1e-3
 gordon_stretch = [0.6, 0.8, 0.95, 1.0, 1.64, 5.0]
 gordon_stress = [0.0, 0.782, 1.0, 1.0, 0.0, 0.0]
+gordon_stress_grid = []
+gordon_start = 0.6
+gordon_end = 1.7
+gordon_nsamples = 220
 ''' fixed parameters '''
 
 def get_gordon(input_stretch):
@@ -41,51 +45,52 @@ def get_gordon(input_stretch):
     slope = (gordon_stress[index] - gordon_stress[index-1])
     slope /= (gordon_stretch[index] - gordon_stretch[index-1])
     return (gordon_stress[index - 1] + slope*(input_stretch - gordon_stretch[index - 1] ))
+    
+for i in range(gordon_nsamples):
+  xvalue = gordon_start + i*(gordon_end - gordon_start)/gordon_nsamples 
+  yvalue = get_gordon(xvalue)
+  gordon_stress_grid += [yvalue]
+
 
 def f(x,a):
-    if(x < 0): return 0
-    if(x <= h): return (f1_0*a*x/h)
-    return 0;
+    return (x >= -TOL and x <= (h+TOL))*(f1_0*a*x/h)
 
 def g(x):
-    if(x<0): return g2 
-    if(x <=h): return (g1*x/h)
-    return (fzah*g1*x/h)
-    
+    return ((x < TOL)*g2 + (x >= TOL and x <= (h+TOL))*(g1*x/h) + (x > (h+TOL))*(fzah*g1*x/h))
+   
+   
 def gordon_correction(input_stretch, nvalue):
-    gord_value = get_gordon(input_stretch)
-    if(gord_value > nvalue): return (gord_value-n)
-    return 0.0     
+    gord_value = tfp.math.interp_regular_1d_grid(x=input_stretch, 
+                   x_ref_min=gordon_start, x_ref_max=gordon_end, 
+                   y_ref=gordon_stress_grid, 
+                   fill_value_below=0,fill_value_above=0)
+    return ((K.eval(gord_value)-n)*(K.eval(gord_value) > nvalue))   
 
 x = sn.Variable('x')
 t = sn.Variable('t')
 
-'''
-a = sn.Variable('a')
-stretch = sn.Variable('stretch')
-stretch_prev = sn.Variable('stretch_prev')
-v = sn.Variable('v')
-'''
+#a = sn.Variable('a')
+#stretch = sn.Variable('stretch')
+#stretch_prev = sn.Variable('stretch_prev')
 
-stretch = 1.0
-stretch_prev = 1.0 
-a = 1.0
-
-n = sn.Functional('n', [t,x], 4*[100], 'tanh')
-    
-L1 = diff(n, t) - (1.0-n)*f(x,a) + n*g(x)
-L1_cor = diff(n, t) + (stretch - stretch_prev)*(L0/dt) * diff(n, x) - gordon_correction(stretch,n)*f(x,a) + n*g(x)
-I1 = (1-sign(t - TOL))*n
+n = sn.Functional('n', [x,t], 8*[20], 'tanh')    
+L1 = diff(n, t) - (1.0-n)*f(x,1.0) + n*g(x)
+#L1_cor = diff(n, t) + (stretch-stretch_prev)*(L0/dt) * diff(n, x) - gordon_correction(stretch,n)*f(x,a) + n*g(x)
+I1 = (t <= TOL )*n
 
 model = sn.SciModel([t,x], [L1, I1]) 
-t_data, x_data = np.meshgrid( np.linspace(0, 2.0, 100), np.linspace(-21.0, 63.0, 100) )
+t_data, x_data = np.meshgrid( np.linspace(0, 0.5, 100), np.linspace(-21.0, 63.0, 100) )
 
-h = model.train([t_data, x_data], 2*['zero'], learning_rate=1e-5, batch_size=512, epochs=30000, stop_loss_value=1e-16)
+h = model.train([t_data, x_data], 2*['zero'], learning_rate=1e-4, batch_size=512, epochs=10000,
+                 stop_loss_value=1e-9, adaptive_weights={'method':'NTK', 'freq':500})
 model.save_weights('../models/model.hdf5')
 
 t_test = np.array([0,0.001,0.002, 0.4])
 x_test = np.arange(-20.8,63,5.2)
-test_sample = np.meshgrid(t_test, x_test ) 
+#a_test = np.array([1.0])
+#stretch_test = np.array([1.0])
+#stretch_prev_test = np.array([1.0])
+test_sample = np.meshgrid(t_test, x_test) 
 prediction = n.eval(model,test_sample)
 
 original_stdout = sys.stdout 
