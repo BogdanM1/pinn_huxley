2d536fdb3a8a8646156586a424e4063b466f23a6
BogdanM1 <dd11eeaann@gmail.com>
2021-11-17 10:00:00 +0000

    gordon curve

--- src/huxpinn.py
+++ src/huxpinn.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys 
 import pandas as pd 
 import tensorflow as tf
+import tensorflow_probability as tfp
 import sciann as sn
 from keras import backend as K
 from sciann.utils.math import diff, sign
@@ -26,45 +27,28 @@ g2 = 209.0
 fzah = 4.0
 L0 = 1100.0
 dt = 1e-3
-gordon_stretch = [0.6, 0.8, 0.95, 1.0, 1.64, 5.0]
-gordon_stress = [0.0, 0.782, 1.0, 1.0, 0.0, 0.0]
-gordon_stress_grid = []
-gordon_start = 0.6
-gordon_end = 1.7
-gordon_nsamples = 220
+grdstretch = [0.6, 0.8, 0.95, 1.0, 1.64, 5.0]
+grdstress = [0.0, 0.782, 1.0, 1.0, 0.0, 0.0]
 ''' fixed parameters '''
 
-def get_gordon(input_stretch):
-    index = 0
-    glen = len(gordon_stretch) 
-    for igord  in range(glen):
-        if(input_stretch <= gordon_stretch[igord]):
-            index = igord
-            break
-    if(index==0): return gordon_stress[index]
-    slope = (gordon_stress[index] - gordon_stress[index-1])
-    slope /= (gordon_stretch[index] - gordon_stretch[index-1])
-    return (gordon_stress[index - 1] + slope*(input_stretch - gordon_stretch[index - 1] ))
-    
-for i in range(gordon_nsamples):
-  xvalue = gordon_start + i*(gordon_end - gordon_start)/gordon_nsamples 
-  yvalue = get_gordon(xvalue)
-  gordon_stress_grid += [yvalue]
+def lininterp(x, x0, x1, y0, y1):
+  return (y0 + (x-x0)*(y1 - y0)/(x1 - x0))
 
+def gordon_correction(stretch, n):
+    cor = (stretch >= grdstretch[0] and stretch < grdstretch[1])*lininterp(stretch, grdstretch[0],grdstretch[1], grdstress[0],grdstress[1]) 
+    cor+= (stretch >= grdstretch[1] and stretch < grdstretch[2])*lininterp(stretch, grdstretch[1],grdstretch[2], grdstress[1],grdstress[2])
+    cor+= (stretch >= grdstretch[2] and stretch < grdstretch[3])*lininterp(stretch, grdstretch[2],grdstretch[3], grdstress[2],grdstress[3])
+    cor+= (stretch >= grdstretch[3] and stretch < grdstretch[4])*lininterp(stretch, grdstretch[3],grdstretch[4], grdstress[3],grdstress[4])
+    return (cor > n)*(cor - n)
 
 def f(x,a):
     return (x >= -TOL and x <= (h+TOL))*(f1_0*a*x/h)
 
 def g(x):
-    return ((x < TOL)*g2 + (x >= TOL and x <= (h+TOL))*(g1*x/h) + (x > (h+TOL))*(fzah*g1*x/h))
-   
-   
-def gordon_correction(input_stretch, nvalue):
-    gord_value = tfp.math.interp_regular_1d_grid(x=input_stretch, 
-                   x_ref_min=gordon_start, x_ref_max=gordon_end, 
-                   y_ref=gordon_stress_grid, 
-                   fill_value_below=0,fill_value_above=0)
-    return ((K.eval(gord_value)-n)*(K.eval(gord_value) > nvalue))   
+    return ((x < TOL)*g2 + 
+            (x >= TOL and x <= (h+TOL))*(g1*x/h) + 
+            (x > (h+TOL))*(fzah*g1*x/h))
+      
 
 x = sn.Variable('x')
 t = sn.Variable('t')
@@ -72,14 +56,20 @@ t = sn.Variable('t')
 #a = sn.Variable('a')
 #stretch = sn.Variable('stretch')
 #stretch_prev = sn.Variable('stretch_prev')
+stretch = 1.0
+stretch_prev = 1.0
+a = 1.0
 
 n = sn.Functional('n', [x,t], 8*[20], 'tanh')    
-L1 = diff(n, t) - (1.0-n)*f(x,1.0) + n*g(x)
+L1 = diff(n, t) - (1.0-n)*f_lin(x,a) + n*g(x)
 #L1_cor = diff(n, t) + (stretch-stretch_prev)*(L0/dt) * diff(n, x) - gordon_correction(stretch,n)*f(x,a) + n*g(x)
 I1 = (t <= TOL )*n
 
 model = sn.SciModel([t,x], [L1, I1]) 
-t_data, x_data = np.meshgrid( np.linspace(0, 0.5, 100), np.linspace(-21.0, 63.0, 100) )
+
+t_data = np.linspace(0, 0.5, 100)
+x_data = np.linspace(-21.0, 63.0, 200)
+t_data, x_data = np.meshgrid( t_data, x_data )
 
 h = model.train([t_data, x_data], 2*['zero'], learning_rate=1e-4, batch_size=512, epochs=10000,
                  stop_loss_value=1e-9, adaptive_weights={'method':'NTK', 'freq':500})
