f1ff0aace3d5d3026596deb8466f12d24dc41585
BogdanM1 <dd11eeaann@gmail.com>
2021-11-19 16:26:36 +0000

    prof. Milos sredio negativni deo x ose

--- src/huxpinnISOM.py
+++ src/huxpinnISOM.py
@@ -38,37 +38,28 @@ def g(x):
 
 x = sn.Variable('x')
 t = sn.Variable('t')
-a = sn.Variable('a')
-stretch = sn.Variable('stretch')
-stretch_prev = sn.Variable('stretch_prev')
 
-
-n = sn.Functional('n', [x,t,a,stretch,stretch_prev], 8*[20], 'tanh')    
-L1 = diff(n, t) + (stretch-stretch_prev)*(L0/dt) * diff(n, x) - gordon_correction(stretch,n)*f(x,a) + n*g(x)
+n = sn.Functional('n', [x,t], 8*[20], 'tanh')    
+L1 = (diff(n, t)  - (1.0-n)*f(x,1.0) + n*g(x) ) * (1+sign(n)) * 0.5
 I1 = (t < TOL )*n
+I2 = (1-sign(n))*n
 
-model = sn.SciModel([x,t,a,stretch,stretch_prev], [L1, I1]) 
+model = sn.SciModel([x,t], [L1, I1, I2]) 
 
 
-x_train = np.arange(-20.8,63,0.65)
-t_train = np.linspace(0, 2.0, 1000)
-a_train = np.array([1.0])
-stretch_train = np.array([1.0])
-stretch_prev_train = np.array([1.0])
+x_train = np.arange(-20.8,63,2.6)
+t_train = np.linspace(0, 1.0, 500)
 
-x_train, t_train, a_train, stretch_train, stretch_prev_train = np.meshgrid( x_train, t_train, a_train, stretch_train, stretch_prev_train )
+x_train, t_train = np.meshgrid( x_train, t_train )
 
-h = model.train([x_train, t_train, a_train, stretch_train, stretch_prev_train], 2*['zero'], learning_rate=1e-4, batch_size=512, epochs=10000,
+h = model.train([x_train, t_train], 3*['zero'], learning_rate=1e-4, batch_size=512, epochs=10000,
                  stop_loss_value=1e-9, adaptive_weights={'method':'NTK', 'freq':500})
 model.save_weights('../models/model.hdf5')
 
 
 x_test = np.arange(-20.8,63,5.2)
 t_test = np.array([0,0.001,0.002, 0.4])
-a_test = np.array([1.0])
-stretch_test = np.array([1.0])
-stretch_prev_test = np.array([1.0])
-test_sample = np.meshgrid(x_test, t_test, a_test, stretch_test, stretch_prev_test) 
+test_sample = np.meshgrid(x_test, t_test) 
 prediction = n.eval(model,test_sample)
 
 original_stdout = sys.stdout 
@@ -77,6 +68,6 @@ with open('../results/test.csv', 'w') as f:
   print('t,x,n')
   for tind in range(len(t_test)):
     for xind in range(len(x_test)):
-      print(t_test[tind],',',x_test[xind],',',prediction[tind][xind][0][0][0])
+      print(t_test[tind],',',x_test[xind],',',prediction[tind][xind])
   sys.stdout = original_stdout
     
